4c4e53c4664aeaade6b5d17b3dcf605cddc2e9fe
Matthew Hurt <mhurt2018@gmail.com>
2024-01-29 18:58:59 -0600

    initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,24 @@
+class User < ApplicationRecord
+  has_many :blogs
+  has_one :profile
+
+  after_create :create_profile
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :username, presence: true, uniqueness: true
+
+  validate :validate_email_unique
+ 
+  # NOTE, this is here just to demonstrate custom validators.
+  # This can be accomplished using built in validators via the following:
+  # validates :email, uniqueness { case_sensitive: false }
+  def validate_email_unique
+    existing = User.where("lower(email) = '#{email.downcase}'")
+
+    if existing.count > 0
+      errors.add(:email, 'is already taken')
+    end
+  end
+end
