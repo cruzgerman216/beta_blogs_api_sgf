e207106ec3919d8f8c6b7b7cb3d0fea5a3f81070
Matthew Hurt <mhurt2018@gmail.com>
2024-02-12 15:56:01 -0600

    add user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  it 'is not valid without a username' do
+    user = build(:user, username: nil)
+    expect(user).to_not be_valid
+  end
+
+  it 'is not valid without an email' do 
+    user = build(:user, email: nil)
+    expect(user).to_not be_valid
+  end
+
+  it 'is not valid without a first_name' do
+    user = build(:user, first_name: nil)
+    expect(user).to_not be_valid
+  end
+
+  it 'is not valid without a last name' do
+    user = build(:user, last_name: nil)
+    expect(user).to_not be_valid
+  end
 end
